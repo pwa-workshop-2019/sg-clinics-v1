d5f9ac0570f92870aded0719559b1136dbcc96f3
jnoorul <noorul.it@gmail.com>
2019-06-12 22:43:30 +0800

    adding build files

---
+++ build/precache-manifest.51adaa3ca40088ca7688bf6c8d546dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a528c2971e11731c260d1efe0a9ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474f08d12797e4b10d01",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "c4eb5a7a483f6a734b50",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "474f08d12797e4b10d01",
+    "url": "/static/js/2.bf9210fe.chunk.js"
+  },
+  {
+    "revision": "c4eb5a7a483f6a734b50",
+    "url": "/static/js/main.121b131a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
