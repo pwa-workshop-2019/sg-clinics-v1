f69bd3b3fb392db0e167e09f4461128d5b824376
jnoorul <noorul.it@gmail.com>
2019-06-14 15:17:44 +0800

    push notification

--- src/booking/AppointmentConfirmation.js
+++ src/booking/AppointmentConfirmation.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'antd';
 import styled from 'styled-components';
 import {Button, KeyValue } from "../common/Atoms";
 import {Link} from "react-router-dom";
+import {urlB64ToUint8Array} from "../utils/cryptoUtil";
 
 const Container = styled.div`
   width: 90%;
@@ -12,36 +13,70 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
-export default function AppointmentConfirmation({offline = true}) {
-  const status = offline ? 'Pending' : 'Confirmed';
-  return (
-    <Container>
-      <KeyValue label="Appointment Status" value={status} />
-      <KeyValue label="Clinic" value="SP Clinic, Sengkang" />
-      <KeyValue label="Doctor" value="Dr Tan" />
-      <KeyValue label="Date" value="15 June 2019" />
-      <KeyValue label="Time" value="10:00 AM" />
-      { !offline && <KeyValue label="Booking Reference" value="13467" /> }
-      {
-        (offline)
-          ?
-          <h5 style={{marginTop: '1rem'}}>
-            It seems you don't have internet connection at the moment.
-            Don't worry, we will book your appointment once you are back online
-            and will send you notification of the appointment status.
-          </h5>
-          :
-          <h5 style={{marginTop: '1rem'}}>
-            Please reach the clinic 15 minutes before your scheduled appointment time.
-          </h5>
+ const vapidPublicKey = 'BJzh06IXJHkZmXUwg5I02lywJMJMxREr0EhCiYOI2iBnzly0KxERUvoFWlpGP9OycWBSboB77lrGI9HzjpHJ7tE';
+
+
+export default class AppointmentConfirmation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {subscribed: false};
+    this.notifyClicked = this.notifyClicked.bind(this);
+  }
+  notifyClicked() {
+    console.log('subscribing for push notification');
+    navigator.serviceWorker.ready.then(registration => {
+      if (!registration.pushManager) {
+        alert("Push Unsupported");
+        return
       }
-      <Row type="flex" justify="center">
-        <Col md={12} xs={24} style={{marginTop: '1rem'}}>
-          <Link to="/">
-            <Button fluid>Home</Button>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
-  );
+      console.log(`vapid public key${this.vapidPublicKey}`);
+      registration.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlB64ToUint8Array(vapidPublicKey),
+        })
+        .then(subscription => fetch("/subscribe",{method: 'POST', body: JSON.stringify(subscription)}))
+        .then(res => this.setState({subscribed: true}))
+        .catch(err => console.error("Push subscription error: ", err))
+    });
+
+  }
+  render() {
+    const {offline = true} = this.props;
+    const status = offline ? 'Pending' : 'Confirmed';
+    return (
+      <Container>
+        <KeyValue label="Appointment Status" value={status} />
+        <KeyValue label="Clinic" value="SP Clinic, Sengkang" />
+        <KeyValue label="Doctor" value="Dr Tan" />
+        <KeyValue label="Date" value="15 June 2019" />
+        <KeyValue label="Time" value="10:00 AM" />
+        { !offline && <KeyValue label="Booking Reference" value="13467" /> }
+        {
+          (offline)
+            ?
+            <div>
+              <h5 style={{marginTop: '1rem'}}>
+                It seems you don't have internet connection at the moment.
+                Don't worry, we will book your appointment once you are back online
+                and will send you notification of the appointment status.
+              </h5>
+              <Button onClick={this.notifyClicked}>Nofify Me</Button>
+            </div>
+            :
+            <h5 style={{marginTop: '1rem'}}>
+              Please reach the clinic 15 minutes before your scheduled appointment time.
+            </h5>
+        }
+        <Row type="flex" justify="center">
+          <Col md={12} xs={24} style={{marginTop: '1rem'}}>
+            <Link to="/">
+              <Button fluid>Home</Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
+
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,16 @@
+/*eslint-disable*/
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+self.addEventListener("push", event => {
+  console.log('notification received'+event.data.text());
+  const pushMessage = event.data.text();
+
+  const body = {
+    title: 'SG Clinics',
+    icon: '/images/icon-64.png',
+    body: pushMessage
+  };
+
+  event.waitUntil(self.registration.showNotification('SG Clinics', body));
+});
