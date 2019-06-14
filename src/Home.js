107981c7f1a37ec61d3b920d528d036d0750e8eb
jnoorul <noorul.it@gmail.com>
2019-06-14 09:37:25 +0800

    cache responsive images

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import AppointmentDetails from './booking/AppointmentDetails';
+import ImageXs from './images/steth-xs.jpg';
+import ImageSm from './images/steth-sm.jpg';
+import ImageMd from './images/steth-md.jpg';
+import ImageLg from './images/steth-lg.jpg';
 
 export default function Home(props) {
   return (
@@ -9,7 +13,7 @@ export default function Home(props) {
         alt="clinic"
         width="100%"
         src="./images/steth-xs.jpg"
-        srcset="./images/steth-xs.jpg 400w, ./images/steth-sm.jpg 775w,./images/steth-md.jpg 1550w, ./images/steth-lg.jpg 3100w"
+        srcset={`${ImageXs} 400w, ${ImageSm} 775w,${ImageMd} 1550w, ${ImageLg} 3100w`}
         sizes="100vw"
       />
       <Row type="flex" justify="center" style={{marginTop: '2rem'}}>
